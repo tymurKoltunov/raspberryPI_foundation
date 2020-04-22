0c56ebf37005b1dbd8576a463ccb581126a500fe
Tymur Koltunov <tymur.koltunov@nure.ua>
2020-04-21 22:34:35 -0400

    added character c3ass

---
+++ Game/character.py
@@ -0,0 +1,70 @@
+class Character:
+
+    # Create a character
+    def __init__(self, name, description):
+        self._name = name
+        self._description = description
+        self._conversation = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def conversation(self):
+        return self._conversation
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @description.setter
+    def description(self, desc):
+        self._description = desc
+
+    @conversation.setter
+    def conversation(self, conv):
+        self._conversation = conv
+
+    # Describe this character
+    def describe(self):
+        print(self._name + " is here!")
+        print(self._description)
+
+    # Talk to this character
+    def talk(self):
+        if self._conversation is not None:
+            print("[" + self._name + " says]: " + self._conversation)
+        else:
+            print(self._name + " doesn't want to talk to you")
+
+    # Fight with this character
+    def fight(self, combat_item):
+        print(self._name + " doesn't want to fight with you")
+        return True
+
+class Enemy(Character):
+
+    def __init__(self, name, description):
+        super().__init__(name, description)
+        self._weakness = None
+
+    @property
+    def weakness(self):
+        return self._weakness
+
+    @weakness.setter
+    def weakness(self, weakness):
+        self._weakness = weakness
+
+    def fight(self, combat_item):
+        if combat_item == self.weakness:
+            print("You fend " + self.name + " off with the " + combat_item)
+            return True
+        else:
+            print(self.name + " crushes you, puny adventurer")
+            return False
